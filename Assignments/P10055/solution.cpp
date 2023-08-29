0e18f062ed449d25a966f14d12c655ebdeb8a20c
CalvinWalmer <112140788+CalvinWalmer@users.noreply.github.com>
2023-08-28 22:54:09 -0500

    Create solution.cpp

---
+++ Assignments/P10055/solution.cpp
@@ -0,0 +1,21 @@
+/**
+* Calvin Walmer
+* CMPS 4883
+* 2023-08-28
+*/
+#include <iostream>
+
+#define endl "\n"
+
+using namespace std;
+
+int main() {
+    int B = 0, A = 0;
+    
+    while (cin >> A >> B) {
+        
+        // your stuff here
+
+    }
+    return 0;
+}
