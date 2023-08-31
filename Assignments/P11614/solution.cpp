223a12f9552998e21fc6604fe4d0a10f27e07f23
CalvinWalmer <112140788+CalvinWalmer@users.noreply.github.com>
2023-08-31 12:03:46 -0500

    completed p11614

--- Assignments/P11614/solution.cpp
+++ Assignments/P11614/solution.cpp
@@ -5,19 +5,32 @@ using namespace std;
 int main()
 {
     int numNums;
-    long long r, c;
+    long long ps, n, check, nthTerm, l, h;
     cin >> numNums;
+
     for(int i = 0; i < numNums; i++)
     {
-        cin >> r;
-        c = 1;
-        do
-        {
-            r -= c;
-            c++;
+        cin >> ps;
+        h = 1414213561;
+        l = 0;
+        n = h;
+
+        while(true){
+            nthTerm = (0.5 * n * (n+1));
+            check = ps - nthTerm;
+            //cout << "PS: " << ps << " N: " << n << " CHECK: " << check << endl;
+            if (check >= 0 and check + nthTerm < (0.5 * (n+1) * (n+2))){
+                cout << n << endl;
+                break;
+            }else if (check <= 0){ //n is too large
+                h = n;
+                n = ((h - l) / 2) + l;
+
+            }else{ //n is too small
+                l = n;
+                n = h - ((h - l) / 2);
+            }
         }
-        while(r >= 0);
-        cout << c - 2 << endl;
     }
     return(0);
 }
