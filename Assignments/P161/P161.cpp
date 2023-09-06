a146d526938bedc2441e617ef52ea203554b54e5
CalvinWalmer <112140788+CalvinWalmer@users.noreply.github.com>
2023-09-06 15:19:51 -0500

    update P161

--- Assignments/P161/P161.cpp
+++ Assignments/P161/P161.cpp
@@ -6,14 +6,15 @@ using namespace std;
 
 int main()
 {
-    int x, c, on, min, time;
+    int x, xPrev, on, minimum, time;
     vector<int> list;    
 
+    xPrev = 0;
+
     while(cin >> x){
-        if(x == 0 && !c){
-            c++;
+        if(x == 0 && x + xPrev != 0){
             time = 0;
-            for(int t = min; t <= 18000; t++){
+            for(int t = minimum; t <= 18000; t++){
                 on = 0;
                 for (int i = 0; i < list.size(); i++){
                     if (t % (2 * list.at(i)) < list.at(i) - 5){
@@ -28,27 +29,23 @@ int main()
 
 
             list.clear();
-            min = 0;
+            minimum = 0;
 
 
             if (time == 0){
                 cout << "Signals fail to synchronise in 5 hours" << endl;
 
             }else{
-                   cout << setw(2) << setfill('0') << time / 3600 << ":"
+                    cout << setw(2) << setfill('0') << time / 3600 << ":"
                     << setw(2) << setfill('0') << (time % 3600) / 60 << ":"
                     << setw(2) << setfill('0') << time % 60 << endl;
             }
         }else{
-            c = 0;
-            if(min == 0){min = x;}
-            if(x < min){min = x;}
+            if(minimum == 0){minimum = x;}
+            if(x < minimum){minimum = x;}
             list.push_back(x);
         }
-        
-        if (c >= 3){
-            break;
-        }
+        xPrev = x;
     };
     return(0);
 }
