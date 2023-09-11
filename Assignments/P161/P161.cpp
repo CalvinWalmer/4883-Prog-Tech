a39780bb976e5ab313354a1682b604c72696d690
CalvinWalmer <cpwalmer0520@my.msutexas.edu>
2023-09-11 12:27:09 -0500

    Solve P161 Update

--- Assignments/P161/P161.cpp
+++ Assignments/P161/P161.cpp
@@ -6,22 +6,24 @@ using namespace std;
 
 int main()
 {
-    int x, xPrev, on, minimum, time;
+    int x, xPrev, xPrevPrev, on, minimum, time;
     vector<int> list;    
 
-    xPrev = 0;
+    xPrev = 1;
+    xPrevPrev = 1;
+    minimum = 0;
 
     while(cin >> x){
-        if(x == 0 && x + xPrev != 0){
+        if(x == 0 && xPrev != 0 && xPrevPrev != 0){
             time = 0;
-            for(int t = minimum; t <= 18000; t++){
+            for(int t = (2 * minimum) - 5; t <= 18000; t++){
                 on = 0;
                 for (int i = 0; i < list.size(); i++){
                     if (t % (2 * list.at(i)) < list.at(i) - 5){
                         on++;
                     }
                 }
-                if (on == list.size()) {
+                if (on == list.size() && t > 0) {
                     time = t;
                     break; 
                 }
@@ -41,10 +43,15 @@ int main()
                     << setw(2) << setfill('0') << time % 60 << endl;
             }
         }else{
-            if(minimum == 0){minimum = x;}
-            if(x < minimum){minimum = x;}
+            if(minimum == 0){
+                minimum = x;
+            }
+            if(x < minimum){
+                minimum = x;
+            }
             list.push_back(x);
         }
+        xPrevPrev = xPrev;
         xPrev = x;
     };
     return(0);
