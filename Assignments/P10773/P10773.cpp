0d1df2d98372afd08a425de13298e6640c4187f1
CalvinWalmer <112140788+CalvinWalmer@users.noreply.github.com>
2023-09-18 14:15:28 -0500

    Solve P10773

--- Assignments/P10773/P10773.cpp
+++ Assignments/P10773/P10773.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
 #include <cmath>
 #include <iomanip>
+#include <string>
 
 using namespace std;
 
 int main(){
-    float to, d, v, u;
+    float to, d, v, u, temp;
+    string end;
 
     cin >> to;
 
@@ -14,11 +16,18 @@ int main(){
         cin >> v;
         cin >> u;
 
-
-        cout << "Case " << i + 1 << ": " << fixed << setprecision(3) <<(d/sqrt(pow(u,2) - pow(v,2))) - (d / u) << endl;
+        cout << "Case " << i + 1 << ": ";
+        temp = (pow(u,2) - pow(v,2));
+        if (temp > 0){
+            temp = (d/sqrt(temp)) - (d / u);
+            if (temp != 0){
+                cout << fixed << setprecision(3) << temp << endl;
+            }else{
+                cout << "can't determine" << endl;
+            }
+        }else{
+            cout << "can't determine" << endl;
+        } 
     }
-
-
-
     return(0);
 }
