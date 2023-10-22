5ed4aca16aeb667296cf32c3b6769f7ccef63da2
CalvinWalmer <cpwalmer0520@my.msutexas.edu>
2023-10-22 17:44:34 -0500

    Create P11280.cpp

---
+++ Assignments/P11280/P11280.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+    while(true){
+        cin >> numCases;
+        cin >> numCities;
+        string cities[4]
+        for(int i = 0; i < numCities; i++){
+            cin >> cities[i];
+        }
+
+        cin >> 
+
+    }
+    
+}
